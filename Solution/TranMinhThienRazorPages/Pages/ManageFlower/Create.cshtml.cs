f68b47c57278cfb9ec10fa251a44f3fa07df3958
thientm27 <denkhotieu1@gmail.com>
2023-06-11 16:08:35 +0700

    validation flower

--- Solution/TranMinhThienRazorPages/Pages/ManageFlower/Create.cshtml.cs
+++ Solution/TranMinhThienRazorPages/Pages/ManageFlower/Create.cshtml.cs
@@ -17,6 +17,7 @@ namespace RazorPage.Pages.ManageFlower
         private readonly IFlowerBouquetRepository flowerBouquetRepository = new FlowerBouquetRepository();
         private readonly ICategoryRepository categoryRepository = new CategoryRepository();
         private readonly ISupplierRepository supplierRepository = new SupplierRepository();
+
         public IActionResult OnGet()
         {
             var loginUser = HttpContext.Session.GetObjectFromJson<Customer>("user");
@@ -25,14 +26,14 @@ namespace RazorPage.Pages.ManageFlower
             {
                 return RedirectToPage("../Login/Login");
             }
+
             ViewData["CategoryId"] = new SelectList(categoryRepository.GetAllCategory(), "CategoryId", "CategoryName");
-      
+
             ViewData["SupplierId"] = new SelectList(supplierRepository.GetAllSupplier(), "SupplierId", "SupplierName");
             return Page();
         }
 
-        [BindProperty]
-        public FlowerBouquet FlowerBouquet { get; set; }
+        [BindProperty] public FlowerBouquet FlowerBouquet { get; set; }
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
@@ -41,8 +42,61 @@ namespace RazorPage.Pages.ManageFlower
             {
                 return Page();
             }
+
+            if (!CheckValidation(FlowerBouquet))
+            {
+                ViewData["CategoryId"] =
+                    new SelectList(categoryRepository.GetAllCategory(), "CategoryId", "CategoryName");
+
+                ViewData["SupplierId"] =
+                    new SelectList(supplierRepository.GetAllSupplier(), "SupplierId", "SupplierName");
+
+                return Page();
+            }
+
             flowerBouquetRepository.CreateFlower(FlowerBouquet);
             return RedirectToPage("./Index");
+        }
+
+        private bool CheckValidation(FlowerBouquet flowerBouquet)
+        {
+            if (flowerBouquet.UnitsInStock < 0)
+            {
+                ModelState.AddModelError(string.Empty, "UnitsInStock must be >= 0");
+                return false;
+            }
+
+            if (flowerBouquet.UnitPrice <= 0)
+            {
+                ModelState.AddModelError(string.Empty, "UnitPrice must be >= 0");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(flowerBouquet.FlowerBouquetName))
+            {
+                ModelState.AddModelError(nameof(flowerBouquet.FlowerBouquetName), "FlowerBouquetName is required");
+                return false;
+            }
+            else if (flowerBouquet.FlowerBouquetName.Length > 100)
+            {
+                ModelState.AddModelError(nameof(flowerBouquet.FlowerBouquetName),
+                    "FlowerBouquetName cannot exceed 100 characters");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(flowerBouquet.Description))
+            {
+                flowerBouquet.Description = "";
+            }
+            if (!string.IsNullOrEmpty(flowerBouquet.Description) && flowerBouquet.Description.Length > 500)
+            {
+                ModelState.AddModelError(nameof(flowerBouquet.Description), "Description cannot exceed 500 characters");
+                return false;
+            }
+            
+            // Add more validation rules as needed
+
+            return true;
         }
     }
 }
