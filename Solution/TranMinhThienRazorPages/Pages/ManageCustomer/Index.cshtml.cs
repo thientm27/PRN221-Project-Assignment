89e24548e7e96a44beb8a0eead2501fddaabcfce
thientm27 <denkhotieu1@gmail.com>
2023-06-11 15:13:26 +0700

    customer search

--- Solution/TranMinhThienRazorPages/Pages/ManageCustomer/Index.cshtml.cs
+++ Solution/TranMinhThienRazorPages/Pages/ManageCustomer/Index.cshtml.cs
@@ -14,21 +14,42 @@ namespace RazorPage.Pages.ManageCustomer
 {
     public class IndexModel : PageModel
     {
-
         private readonly ICustomerRepository customerRepository = new CustomerRepository();
 
 
-        public IList<Customer> Customer { get;set; }
+        public IList<Customer> Customer { get; set; }
+        [BindProperty] public string SearchValue { get; set; }
 
-        public  IActionResult OnGet()
+        public IActionResult OnGet()
         {
+            SearchValue = "";
             var loginUser = HttpContext.Session.GetObjectFromJson<Customer>("user");
 
             if (loginUser == null || loginUser.CustomerId != -1) // not login or not admin
             {
                 return RedirectToPage("../Login/Login");
             }
+
             Customer = customerRepository.GetAllCustomer();
+            return Page();
+        }
+
+        public IActionResult OnPostSearch()
+        {
+            if (string.IsNullOrEmpty(SearchValue))
+            {
+                Customer = customerRepository.GetAllCustomer();
+                return Page();
+            }
+
+            var search = SearchValue.ToUpper().Trim();
+            Customer = customerRepository.GetAllCustomer()
+                .Where(O => O.CustomerName.ToUpper().Trim().Contains(search)
+                            || O.CustomerName.ToUpper().Trim().Contains(search)
+                            || O.City.ToUpper().Trim().Contains(search)
+                            || O.Country.ToUpper().Trim().Contains(search)
+                            || O.Email.ToUpper().Trim().Contains(search)
+                ).ToList();
             return Page();
         }
     }
